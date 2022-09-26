a3ee8251d41c5e917e828ee3c5a736393350a7e8
Protesilaos Stavrou <info@protesilaos.com>
2022-09-26 23:20:29 +0300

    Start recording file history

---
+++ agitate.el
@@ -0,0 +1,39 @@
+;;; agitate.el --- WORK-IN-PROGRESS -*- lexical-binding: t -*-
+
+;; Copyright (C) 2022  Free Software Foundation, Inc.
+
+;; Author: Protesilaos Stavrou <info@protesilaos.com>
+;; Maintainer: NOT-YET-AVAILABLE Development <~protesilaos/NOT-YET-AVAILABLE@lists.sr.ht>
+;; URL: https://git.sr.ht/~protesilaos/NOT-YET-AVAILABLE
+;; Mailing-List: https://lists.sr.ht/~protesilaos/NOT-YET-AVAILABLE
+;; Version: 0.0.0
+;; Package-Requires: ((emacs "27.1"))
+;; Keywords: convenience, version control, git
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Another Git Interface Trying to Agitate Tranquil Emacsers.
+
+;;; Code:
+
+(defgroup agitate ()
+  "Work-in-progress."
+  :group 'vc)
+
+(provide 'agitate)
+;;; agitate.el ends here
