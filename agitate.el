27abc812e555eeccf00d559b56f77f5e25df2391
Protesilaos Stavrou <info@protesilaos.com>
2024-01-12 08:25:46 +0200

    Update copyright years

--- agitate.el
+++ agitate.el
@@ -1,6 +1,6 @@
 ;;; agitate.el --- Extras for diff-mode, vc-git, log-edit, log-view -*- lexical-binding: t -*-
 
-;; Copyright (C) 2022-2023  Free Software Foundation, Inc.
+;; Copyright (C) 2022-2024  Free Software Foundation, Inc.
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Agitate Development <~protesilaos/agitate@lists.sr.ht>
